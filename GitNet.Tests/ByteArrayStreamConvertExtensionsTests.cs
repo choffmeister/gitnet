5d5519f198078431afad93660cfe18de0e5b238a
choffmeister <thekwasti@googlemail.com>
2012-03-01 15:09:29 +0100

    activated automatisch NuGet package downloading added NUnit package for tests create ByteArrayStreamConverterExtensions

---
+++ GitNet.Tests/ByteArrayStreamConvertExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GitNet.Tests
+{
+    [TestFixture]
+    public class ByteArrayStreamConvertExtensionsTests
+    {
+        [Test]
+        public void ToByteArrayInvertsToStream()
+        {
+            byte[] start8192 = new byte[] { 1, 2, 3, 4, 5, 6 };
+            byte[] end8192 = start8192.ToStream().ToByteArray();
+
+            Assert.AreEqual(start8192, end8192);
+
+            byte[] start1 = new byte[] { 1, 2, 3, 4, 5, 6 };
+            byte[] end1 = start1.ToStream(1).ToByteArray(1);
+
+            Assert.AreEqual(start1, end1);
+        }
+    }
+}
---
+++ GitNet/ByteArrayStreamConvertExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GitNet
+{
+    public static class ByteArrayStreamConvertExtensions
+    {
+        public static Stream ToStream(this byte[] byteArray, int blockSize = 8192)
+        {
+            byte[] newByteArray = new byte[byteArray.Length];
+
+            int i = 0;
+            while (i < byteArray.Length)
+            {
+                int toWrite = Math.Min(byteArray.Length - i, blockSize);
+                Array.Copy(byteArray, i, newByteArray, i, toWrite);
+                i += toWrite;
+            }
+
+            return new MemoryStream(newByteArray);
+        }
+
+        public static byte[] ToByteArray(this Stream stream, int blockSize = 8192)
+        {
+            List<Tuple<byte[], int>> buffers = new List<Tuple<byte[], int>>();
+
+            int acutallyRead = -1;
+            while (acutallyRead != 0)
+            {
+                byte[] buffer = new byte[blockSize];
+                acutallyRead = stream.Read(buffer, 0, blockSize);
+                buffers.Add(Tuple.Create(buffer, acutallyRead));
+            }
+
+            int i = 0;
+            int j = 0;
+            byte[] byteArray = new byte[buffers.Sum(n => n.Item2)];
+
+            while (i < buffers.Count)
+            {
+                Array.Copy(buffers[i].Item1, 0, byteArray, j, buffers[i].Item2);
+                j += buffers[i].Item2;
+                i += 1;
+            }
+
+            return byteArray;
+        }
+    }
+}
