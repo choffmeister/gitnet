c76ce5cf34f6dbd69c963cfe0412fb8d01adfbb3
choffmeister <thekwasti@googlemail.com>
2012-03-02 10:22:55 +0100

    implemented caching of already parsed objects

--- GitNet/GitRepository.cs
+++ GitNet/GitRepository.cs
@@ -1,19 +1,32 @@
-﻿using GitNet.VirtualizedGitFolder;
+﻿using System.Collections.Generic;
+using GitNet.VirtualizedGitFolder;
 
 namespace GitNet
 {
     public class GitRepository
     {
         private readonly IGitFolder _gitFolder;
+        private readonly Dictionary<GitObjectId, GitObject> _cache;
 
         public GitRepository(IGitFolder gitFolder)
         {
             _gitFolder = gitFolder;
+            _cache = new Dictionary<GitObjectId, GitObject>();
         }
 
         public GitObject Lookup(GitObjectId id)
         {
-            return GitObject.CreateFromRaw(id, _gitFolder.ReadFile("objects/" + id.Sha.Substring(0, 2) + "/" + id.Sha.Substring(2)));
+            if (_cache.ContainsKey(id))
+            {
+                return _cache[id];
+            }
+            else
+            {
+                GitObject go = GitObject.CreateFromRaw(id, _gitFolder.ReadFile("objects/" + id.Sha.Substring(0, 2) + "/" + id.Sha.Substring(2)));
+                _cache[id] = go;
+
+                return go;
+            }
         }
     }
 }
